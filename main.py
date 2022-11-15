8773524c7ae2364d1a0f6d433ca585249dcb9680
Toby Mortimer <tobymortimer@Tobys-MacBook-Air.local>
2022-11-15 10:22:29 +0000

    helo

--- main.py
+++ main.py
@@ -23,9 +23,17 @@ def netball():
 def formula1():
     return render_template('formula1.html')
 
+@app.route("/w-series")
+def wseries():
+    return render_template('W-series.html')
+
 @app.route("/football")
 def football():
     return render_template('football.html')
+
+@app.route("/tennis")
+def tennis():
+    return render_template('tennis.html')
 
 if __name__ == '__main__':
     app.run(host="localhost", debug=True, port=9000)
